a4e98631924e6e8b8fa691d86c4a1991bd34dae6
Keyur <keyur@getmagnet.com>
2016-01-12 15:37:07 +0530

    Paypal add subject

--- index.js
+++ index.js
@@ -14,7 +14,20 @@ function Paypal(username, password, signature, returnUrl, cancelUrl, debug) {
 	this.redirect = 'https://' + (debug ? 'www.sandbox.paypal.com/cgi-bin/webscr' : 'www.paypal.com/cgi-bin/webscr');
 };
 
-Paypal.prototype.params = function() {
+function Paypal(username, password, signature, returnUrl, cancelUrl, debug, subject) {
+	this.username = username;
+	this.password = password;
+	this.solutiontype = 'Mark';
+	this.signature = signature;
+	this.debug = debug || false;
+	this.returnUrl = returnUrl;
+	this.cancelUrl = cancelUrl;
+	this.url = 'https://' + (debug ? 'api-3t.sandbox.paypal.com' : 'api-3t.paypal.com') + '/nvp';
+	this.redirect = 'https://' + (debug ? 'www.sandbox.paypal.com/cgi-bin/webscr' : 'www.paypal.com/cgi-bin/webscr');
+	this.subject = subject;
+};
+
+Paypal.prototype.params = function () {
 	var self = this;
 	return {
 		USER: self.username,
@@ -25,9 +38,9 @@ Paypal.prototype.params = function() {
 	};
 };
 
-Paypal.prototype.detail = function(token, payer, callback) {
+Paypal.prototype.detail = function (token, payer, callback) {
 
-	if (token.get !== undefined && typeof(payer) === 'function') {
+	if (token.get !== undefined && typeof (payer) === 'function') {
 		callback = payer;
 		payer = token.get.PayerID;
 		token = token.get.token;
@@ -39,14 +52,14 @@ Paypal.prototype.detail = function(token, payer, callback) {
 	params.TOKEN = token;
 	params.METHOD = 'GetExpressCheckoutDetails';
 
-	self.request(self.url, 'POST', params, function(err, data) {
+	self.request(self.url, 'POST', params, function (err, data) {
 
 		if (err) {
 			callback(err, data);
 			return;
 		}
 
-		if (typeof(data.CUSTOM) === 'undefined') {
+		if (typeof (data.CUSTOM) === 'undefined') {
 			callback(data, null);
 			return;
 		}
@@ -61,7 +74,7 @@ Paypal.prototype.detail = function(token, payer, callback) {
 		params.CURRENCYCODE = custom[2];
 		params.METHOD = 'DoExpressCheckoutPayment';
 
-		self.request(self.url, 'POST', params, function(err, data) {
+		self.request(self.url, 'POST', params, function (err, data) {
 
 			if (err) {
 				callback(err, data);
@@ -75,10 +88,10 @@ Paypal.prototype.detail = function(token, payer, callback) {
 	return self;
 };
 
-Paypal.prototype.pay = function(invoiceNumber, amount, description, currency, requireAddress, callback) {
+Paypal.prototype.pay = function (invoiceNumber, amount, description, currency, requireAddress, callback) {
 
 	// Backward compatibility
-	if (typeof(requireAddress) === 'function') {
+	if (typeof (requireAddress) === 'function') {
 		callback = requireAddress;
 		requireAddress = false;
 	}
@@ -98,7 +111,7 @@ Paypal.prototype.pay = function(invoiceNumber, amount, description, currency, re
 	params.INVNUM = invoiceNumber;
 	params.CUSTOM = invoiceNumber + '|' + params.AMT + '|' + currency;
 
-	self.request(self.url, 'POST', params, function(err, data) {
+	self.request(self.url, 'POST', params, function (err, data) {
 
 		if (err) {
 			callback(err, null);
@@ -116,7 +129,49 @@ Paypal.prototype.pay = function(invoiceNumber, amount, description, currency, re
 	return self;
 };
 
-Paypal.prototype.request = function(url, method, data, callback) {
+Paypal.prototype.pay = function (invoiceNumber, amount, description, currency, requireAddress, subject, callback) {
+
+	// Backward compatibility
+	if (typeof (requireAddress) === 'function') {
+		callback = requireAddress;
+		requireAddress = false;
+	}
+
+	var self = this;
+	var params = self.params();
+
+	params.PAYMENTACTION = 'Sale';
+	params.AMT = prepareNumber(amount);
+	params.RETURNURL = self.returnUrl;
+	params.CANCELURL = self.cancelUrl;
+	params.DESC = description;
+	params.NOSHIPPING = requireAddress ? 0 : 1;
+	params.ALLOWNOTE = 1;
+	params.CURRENCYCODE = currency;
+	params.METHOD = 'SetExpressCheckout';
+	params.INVNUM = invoiceNumber;
+	params.CUSTOM = invoiceNumber + '|' + params.AMT + '|' + currency;
+	params.SUBJECT = subject;
+
+	self.request(self.url, 'POST', params, function (err, data) {
+
+		if (err) {
+			callback(err, null);
+			return;
+		}
+
+		if (data.ACK === 'Success') {
+			callback(null, self.redirect + '?cmd=_express-checkout&useraction=commit&token=' + data.TOKEN);
+			return;
+		}
+
+		callback(new Error('ACK ' + data.ACK + ': ' + data.L_LONGMESSAGE0), null);
+	});
+
+	return self;
+};
+
+Paypal.prototype.request = function (url, method, data, callback) {
 
 	var self = this;
 	var params = Qs.stringify(data);
@@ -131,20 +186,29 @@ Paypal.prototype.request = function(url, method, data, callback) {
 	headers['Content-Length'] = params.length;
 
 	var location = '';
-	var options = { protocol: uri.protocol, auth: uri.auth, method: method || 'GET', hostname: uri.hostname, port: uri.port, path: uri.path, agent: false, headers: headers };
+	var options = {
+		protocol: uri.protocol,
+		auth: uri.auth,
+		method: method || 'GET',
+		hostname: uri.hostname,
+		port: uri.port,
+		path: uri.path,
+		agent: false,
+		headers: headers
+	};
 
 	var response = function (res) {
 		var buffer = '';
 
-		res.on('data', function(chunk) {
+		res.on('data', function (chunk) {
 			buffer += chunk.toString('utf8');
 		})
 
-		req.setTimeout(exports.timeout, function() {
+		req.setTimeout(exports.timeout, function () {
 			callback(new Error('timeout'), null);
 		});
 
-		res.on('end', function() {
+		res.on('end', function () {
 
 			var error = null;
 			var data = '';
@@ -161,8 +225,8 @@ Paypal.prototype.request = function(url, method, data, callback) {
 
 	var req = Https.request(options, response);
 
-	req.on('error', function(err) {
-    	callback(err, null);
+	req.on('error', function (err) {
+		callback(err, null);
 	});
 
 	if (method === 'POST')
@@ -193,10 +257,10 @@ function prepareNumber(num, doubleZero) {
 exports.timeout = 10000;
 exports.Paypal = Paypal;
 
-exports.init = function(username, password, signature, returnUrl, cancelUrl, debug) {
+exports.init = function (username, password, signature, returnUrl, cancelUrl, debug) {
 	return new Paypal(username, password, signature, returnUrl, cancelUrl, debug);
 };
 
-exports.create = function(username, password, signature, returnUrl, cancelUrl, debug) {
+exports.create = function (username, password, signature, returnUrl, cancelUrl, debug) {
 	return exports.init(username, password, signature, returnUrl, cancelUrl, debug);
 };
